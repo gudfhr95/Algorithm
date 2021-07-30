833cda3b1bab4d9595d60eef26549ca6c4ef8e3f
Hyungrok Ham <gudfhr95@kaist.ac.kr>
2021-07-30 21:34:14 +0900

    [BOJ 2440] 별 찍기 - 3

---
+++ BOJ/2440.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+
+  string s;
+  for (int i = N; i > 0; i--) {
+    s = "";
+    for (int j = 0; j < i; j++) {
+      s += "*";
+    }
+    cout << s << endl;
+  }
+
+  return 0;
+}
