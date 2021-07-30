a3c43a9252924dfc3c81102b3da1ea96699d9c57
Hyungrok Ham <gudfhr95@kaist.ac.kr>
2021-07-30 20:40:51 +0900

    [BOJ 10162] 전자레인지

---
+++ BOJ/10162.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int T;
+
+  cin >> T;
+
+  if (T % 10 != 0) {
+    cout << -1;
+  } else {
+    cout << T / 300 << " ";
+    T %= 300;
+    cout << T / 60 << " ";
+    T %= 60;
+    cout << T / 10;
+  }
+}
