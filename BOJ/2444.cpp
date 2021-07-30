eafd998cc8f39bd8a09e08c203bef0a8989bc059
Hyungrok Ham <gudfhr95@kaist.ac.kr>
2021-07-30 22:33:24 +0900

    [BOJ 2444] 별 찍기 - 7

---
+++ BOJ/2444.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int N;
+  string s;
+
+  cin >> N;
+
+  for (int i = 0; i < N; i++) {
+    s = "";
+
+    for (int j = 0; j < N - i - 1; j++) {
+      s += ' ';
+    }
+
+    for (int j = 0; j < i; j++) {
+      s += '*';
+    }
+
+    s += '*';
+
+    for (int j = 0; j < i; j++) {
+      s += '*';
+    }
+
+    cout << s << endl;
+  }
+
+  for (int i = N - 2; i >= 0; i--) {
+    s = "";
+
+    for (int j = 0; j < N - i - 1; j++) {
+      s += ' ';
+    }
+
+    for (int j = 0; j < i; j++) {
+      s += '*';
+    }
+
+    s += '*';
+
+    for (int j = 0; j < i; j++) {
+      s += '*';
+    }
+
+    cout << s << endl;
+  }
+
+  return 0;
+}
