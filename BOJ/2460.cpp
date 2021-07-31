93208c380989a5af538d17f160e21ecd620e15a1
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-07-31 15:33:58 +0900

    [BOJ 2460] 지능형 기차 2

---
+++ BOJ/2460.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int in, out, total = 0, max = 0;
+
+  for (int i = 0; i < 10; i++) {
+    cin >> out >> in;
+    total = clamp(total + in - out, 0, 10000);
+    if (total > max) max = total;
+  }
+
+  cout << max;
+}
