8ec07ea621a8cb98d51f8a205d29291c6fe85f14
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-08-01 16:43:32 +0900

    [BOJ 11721] 열 개씩 끊어 출력하기

---
+++ BOJ/11721.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  string s;
+
+  cin >> s;
+
+  for (int i = 0; i < s.length(); i++) {
+    cout << s[i];
+    if (i % 10 == 9) cout << '\n';
+  }
+
+  return 0;
+}
