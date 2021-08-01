8cb745bc67a66d516c1651a266f73d0f193b3301
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-08-01 17:01:20 +0900

    [BOJ 9086] 문자열

---
+++ BOJ/9086.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int T;
+  string s;
+
+  cin >> T;
+
+  while (T--) {
+    cin >> s;
+
+    cout << s[0] << s[s.length() - 1] << '\n';
+  }
+}
