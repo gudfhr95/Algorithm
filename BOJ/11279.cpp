5d684254b1fcca89cce2c731f2e04c96eb2d7007
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-08-11 20:12:20 +0900

    [BOJ 11279] 최대 힙

---
+++ BOJ/11279.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+  int N, x;
+  priority_queue<int> pq;
+
+  // cin, cout 빠르게하기
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  cin >> N;
+
+  while (N--) {
+    cin >> x;
+
+    if (x == 0) {
+      if (pq.empty())
+        cout << 0 << '\n';
+      else {
+        cout << pq.top() << '\n';
+        pq.pop();
+      }
+    } else {
+      pq.push(x);
+    }
+  }
+}
