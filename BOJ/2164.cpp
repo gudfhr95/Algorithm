2349883a16709a1ab642caee2da2b95818c11a99
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-08-17 21:10:04 +0900

    [BOJ 2164] 카드2

---
+++ BOJ/2164.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+void drop(queue<int> &cards) {
+  cards.pop();
+  cards.push(cards.front());
+  cards.pop();
+}
+
+int main() {
+  int N;
+  queue<int> cards;
+
+  cin >> N;
+
+  for (int i = 1; i <= N; i++) {
+    cards.push(i);
+  }
+
+  while (cards.size() > 1) {
+    drop(cards);
+  }
+
+  cout << cards.front();
+}
