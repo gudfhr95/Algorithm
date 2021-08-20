a9b616d66d6892dbdfe17af46452324894b8b54d
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-08-20 21:23:04 +0900

    [BOJ 5691] 평균 중앙값 문제

---
+++ BOJ/5691.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  long long a, b;
+
+  while (true) {
+    cin >> a >> b;
+
+    if (a == 0 && b == 0) break;
+
+    cout << a - (b - a) << '\n';
+  }
+}
