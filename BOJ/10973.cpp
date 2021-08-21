730648163be92af282fad8d97dfaf5b99087b919
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-08-21 12:09:28 +0900

    [BOJ 10973] 이전 순열

---
+++ BOJ/10973.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int N, n;
+  vector<int> numbers;
+
+  cin >> N;
+
+  for (int i = 0; i < N; i++) {
+    cin >> n;
+    numbers.push_back(n);
+  }
+
+  if (!prev_permutation(numbers.begin(), numbers.end())) {
+    cout << -1;
+  } else {
+    for (const auto &elem : numbers) {
+      cout << elem << ' ';
+    }
+  }
+}
