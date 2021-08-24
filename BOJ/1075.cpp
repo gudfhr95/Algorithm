3c07e2ea0bbdac41ad8f5e8ab25acbe2439447a5
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-08-24 21:51:57 +0900

    [BOJ 1075] 나누기

---
+++ BOJ/1075.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int N, F;
+
+  cin >> N >> F;
+
+  N = (N / 100) * 100;
+  for (int i = 0; i < 100; i++) {
+    if ((N + i) % F == 0) {
+      printf("%02d", i);
+      break;
+    }
+  }
+}
