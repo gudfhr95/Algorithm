85f888b803e12569d66f3d45a0fbd7453ef446b9
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-09-18 15:07:33 +0900

    [BOJ 17087] 숨바꼭질 6

---
+++ BOJ/17087.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int gcd(int a, int b) { return (a % b == 0) ? b : gcd(b, a % b); }
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int N, S;
+
+  cin >> N >> S;
+
+  int A[N];
+  for (int i = 0; i < N; i++) {
+    cin >> A[i];
+  }
+
+  for (int i = 0; i < N; i++) {
+    A[i] = abs(S - A[i]);
+  }
+
+  int result = A[0];
+  for (int i = 1; i < N; i++) {
+    result = gcd(result, A[i]);
+  }
+
+  cout << result;
+}
