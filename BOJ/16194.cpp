a75a39219df0cb5bddba441082e1ed0e13fe239d
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-09-20 10:22:40 +0900

    [BOJ 16194] 카드 구매하기 2

---
+++ BOJ/16194.cpp
@@ -0,0 +1,35 @@
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+int cache[1001];
+int price[10001];
+
+int buy(int n) {
+  if (cache[n] != -1) return cache[n];
+
+  int &ret = cache[n];
+
+  ret = price[n];
+  for (int i = 1; i <= n; i++) {
+    int p = price[i] + buy(n - i);
+    ret = min(ret, p);
+  }
+
+  return ret;
+}
+
+int main() {
+  int N;
+
+  cin >> N;
+
+  memset(cache, -1, sizeof(cache));
+
+  for (int i = 1; i <= N; i++) {
+    cin >> price[i];
+  }
+
+  cout << buy(N);
+}
