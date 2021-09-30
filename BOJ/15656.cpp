17523c6c0651cf0b9f56dc63fc0767c224e589f6
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-09-30 20:26:49 +0900

    [BOJ 15656] N과 M (7)

---
+++ BOJ/15656.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int seq[8];
+bool c[8];
+int a[8];
+
+void permutation(int index, int n, int m) {
+  if (index == m) {
+    for (int i = 0; i < m; i++) {
+      cout << a[i] << ' ';
+    }
+    cout << '\n';
+    return;
+  }
+
+  for (int i = 0; i < n; i++) {
+    c[i] = true;
+
+    a[index] = seq[i];
+    permutation(index + 1, n, m);
+    c[i] = false;
+  }
+}
+
+int main() {
+  int N, M;
+
+  cin >> N >> M;
+
+  for (int i = 0; i < N; i++) {
+    cin >> seq[i];
+  }
+
+  sort(seq, seq + N);
+
+  permutation(0, N, M);
+}
