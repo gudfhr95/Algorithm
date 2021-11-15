f892dc7d1d9096fe432747304e0989611ad04e3c
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-11-15 12:58:27 +0900

    [BOJ 1783] 병든 나이트

---
+++ BOJ/1783.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int N, M;
+
+  cin >> N >> M;
+
+  if (N == 1) {
+    cout << 1;
+  } else if (N == 2) {
+    cout << min(4, (M + 1) / 2);
+  } else {
+    if (M < 7) {
+      cout << min(4, M);
+    } else {
+      cout << M - 2;
+    }
+  }
+}
