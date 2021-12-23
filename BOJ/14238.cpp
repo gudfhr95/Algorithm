b89eb0efa1cd6a1b78bc7d27481654ed24628625
Hyungrok Ham <hyungrok_ham@tmax.co.kr>
2021-12-23 10:58:45 +0900

    [BOJ 14238] 출근 기록

---
+++ BOJ/14238.cpp
@@ -0,0 +1,77 @@
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+int d[51][51][51][3][3];
+
+int go(int a, int b, int c, int p1, int p2) {
+  int &ret = d[a][b][c][p1][p2];
+  if (a + b + c == 0) {
+    return ret = 1;
+  }
+
+  if (ret != -1) return ret;
+
+  if (a > 0 && go(a - 1, b, c, 0, p1) == 1) {
+    return ret = 1;
+  }
+
+  if (b > 0 && p1 != 1) {
+    if (go(a, b - 1, c, 1, p1) == 1) {
+      return ret = 1;
+    }
+  }
+
+  if (c > 0 && p1 != 2 && p2 != 2) {
+    if (go(a, b, c - 1, 2, p1) == 1) {
+      return ret = 1;
+    }
+  }
+
+  return ret = 0;
+}
+
+string back(int a, int b, int c, int p1, int p2) {
+  if (a + b + c == 0) {
+    return "";
+  }
+
+  if (a > 0 && go(a - 1, b, c, 0, p1) == 1) {
+    return "A" + back(a - 1, b, c, 0, p1);
+  }
+
+  if (b > 0 && p1 != 1) {
+    if (go(a, b - 1, c, 1, p1) == 1) {
+      return "B" + back(a, b - 1, c, 1, p1);
+    }
+  }
+
+  if (c > 0 && p1 != 2 && p2 != 2) {
+    if (go(a, b, c - 1, 2, p1) == 1) {
+      return "C" + back(a, b, c - 1, 2, p1);
+    }
+  }
+
+  return "";
+}
+
+int main() {
+  string S;
+
+  cin >> S;
+
+  int limit[3] = {0, 0, 0};
+  for (char c : S) {
+    limit[c - 'A'] += 1;
+  }
+
+  memset(d, -1, sizeof(d));
+
+  int result = go(limit[0], limit[1], limit[2], 0, 0);
+  if (result == 0) {
+    cout << -1;
+  } else {
+    cout << back(limit[0], limit[1], limit[2], 0, 0);
+  }
+}
