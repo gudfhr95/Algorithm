9c6652830390d036864c86a91e9eee355e97c578
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-12-24 23:43:49 +0900

    [BOJ 2252] 줄 세우기

---
+++ BOJ/2252.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+vector<int> adj[32001];
+int indegree[32001];
+
+void bfs() {
+  queue<int> q;
+  for (int i = 1; i <= N; i++) {
+    if (indegree[i] == 0) {
+      q.push(i);
+    }
+  }
+
+  while (!q.empty()) {
+    int cur = q.front();
+    q.pop();
+
+    cout << cur << ' ';
+
+    for (int next : adj[cur]) {
+      indegree[next] -= 1;
+
+      if (indegree[next] == 0) {
+        q.push(next);
+      }
+    }
+  }
+}
+
+int main() {
+  cin >> N >> M;
+
+  int A, B;
+  for (int i = 0; i < M; i++) {
+    cin >> A >> B;
+
+    adj[A].push_back(B);
+    indegree[B] += 1;
+  }
+
+  bfs();
+}
