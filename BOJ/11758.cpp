52e6b2a9f7787d41455ae9cc944ca2c5638c1a33
Hyungrok Ham <hyungrok_ham@tmax.co.kr>
2021-12-24 15:41:19 +0900

    [BOJ 11758] CCW

---
+++ BOJ/11758.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int x1, y1, x2, y2, x3, y3;
+
+  cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+
+  int result = x1 * y2 + x2 * y3 + x3 * y1 - y1 * x2 - y2 * x3 - y3 * x1;
+
+  if (result > 0) {
+    cout << 1;
+  } else if (result == 0) {
+    cout << 0;
+  } else {
+    cout << -1;
+  }
+}
